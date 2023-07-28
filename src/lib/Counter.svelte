a6ba7a3815140cbb9ceeb19749df346ea4fa5be0
Neto Hog <neto@bornlogic.com>
2023-07-28 15:42:46 -0300

    first commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,12 @@
+<svelte:options customElement="sbelte-counter" />
+
+<script>
+  let count = 0;
+  const increment = () => {
+    count += 1;
+  };
+</script>
+
+<button on:click={increment}>
+  {count} 🍊
+</button>
